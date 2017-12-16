74988553de8712b9753fddbaf144c66ee8dc26c9
Blake Harris <harrisblake85@gmail.com>
2017-12-16 14:22:17 -0700

    Toggle Game Infrastructure added

--- public/js/app.js
+++ public/js/app.js
@@ -3,5 +3,13 @@ const app = angular.module("Profile-GamesApp",[]);
 app.controller("MainController",["$http",function ($http) {
   //begin MainController
   this.hello="Hello World";
+  this.showgame=true;
+  //togglegame doesnt work yet sorry, will work soon
+  this.toggleGame = () => {
+    if (this.showgame) {
+      this.showgame=false;
+    }else {
+      this.showgame=true;
+    }}
   //end MainController
 }]);
