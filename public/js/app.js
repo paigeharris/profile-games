e3d2185e3b7961b56ee8a8529f171f55ef6d336d
hector guevara <hectorguevara@hectors-MacBook-Pro.local>
2017-12-16 22:25:06 -0600

    workign on app.js

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,12 @@ const app = angular.module("Profile-GamesApp",[]);
 app.controller("MainController",["$http",function ($http) {
   //begin MainController
   this.hello="Hello World";
-  this.showgame=true;
-  this.toggleGame = () => {
-    if (this.showgame) {
-      this.showgame=false;
-    }else {
-      this.showgame=true;
-    }}
   //end MainController
+
+//show all users
+this.getUsers= () =>
+
+
+
+
 }]);
