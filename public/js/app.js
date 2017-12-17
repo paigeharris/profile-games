3a6ecb60d4a6994b51ebc70021d5ab4db661d441
Jaq Dunham <jdunham9@gmail.com>
2017-12-17 00:06:59 -0500

    started app.js, user register/login/logout

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,6 @@ const app = angular.module("Profile-GamesApp",[]);
 
 app.controller("MainController",["$http",function ($http) {
   //begin MainController
-  this.hello="Hello World";
   this.showgame=true;
   this.toggleGame = () => {
     if (this.showgame) {
@@ -10,5 +9,41 @@ app.controller("MainController",["$http",function ($http) {
     }else {
       this.showgame=true;
     }}
+
+  // --------------------------------------------
+  // Users/authorization
+  this.user = {};
+  this.error = null;
+
+  this.registerUser = () => {
+    $http({
+      url: '/users',
+      method: 'post',
+      data: this.newUserForm
+    }).then(response => {
+      console.log('Successful registration');
+      this.user = response.data;
+    }, ex => {
+      console.log(ex.data.err);
+      this.error = ex.statusText;
+    }).catch(err => this.error = 'Is server working?');
+  };
+
+  this.loginUser = () => {
+    $http({ url: '/sessions/login',
+    method: 'post',
+    data: this.loginForm })
+  };
+
+  this.logoutUser = () => {
+    $http({
+      url: '/sessions/logout',
+      method: 'delete'
+    }).then((response) => {
+      console.log(response.data);
+      this.user = null;
+    });
+  };
+
   //end MainController
 }]);
