d59d7f0fb0e47f4aa8eba79d1506999c44dd0cb1
hector guevara <hectorguevara@hectors-MacBook-Pro.local>
2017-12-17 18:49:11 -0600

    nav button is working

--- public/js/app.js
+++ public/js/app.js
@@ -2,25 +2,33 @@ const app = angular.module("Profile-GamesApp",[]);
 
 app.controller("MainController",["$http",function ($http) {
   //begin MainController
+  // ctrl variables
   this.hello="Hello World";
   this.showgame=true;
+  this.shownav=false;
+  // ctrl functions
   this.toggleGame = () => {
     if (this.showgame) {
       this.showgame=false;
-    }else {
+    } else {
       this.showgame=true;
     }}
+
+  this.openNav = () => {
+    this.shownav=true;
+  }
+
+  this.closeNav = () => {
+    this.shownav=false;
+  }
   //end MainController
 
 //navbar
- this.openNav() {
-   document.getElementById('mySidenav').style.width= "250px";
- }
 
- this.closeNav(){
-   document.getElementById("mySidenav").style.width="0";
- }
 
+
+
+ //
 
 
 }]);
