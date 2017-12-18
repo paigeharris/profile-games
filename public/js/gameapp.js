a492ea0ee3c32dcf0aaf6cdd89fc10c531b28939
Blake Harris <harrisblake85@gmail.com>
2017-12-18 10:04:00 -0700

    game app

--- public/js/gameapp.js
+++ public/js/gameapp.js
@@ -3,14 +3,24 @@ $(() => {
   //onload
  const $gamebutton = $("<button>"+"Click Me To Score"+"</button>")
  const $game = $("#game")
+ $game.css({
+   width:'800px',
+   height:'600px',
+   "background-color":"gray"
+ })
  $game.append($gamebutton);
  $gamebutton.on("click",() => {
    $gamebutton.hide();
-   var buttonsize = ((Math.random()*100) + 50).toFixed();
-   var color = '#'+ Math.round(0xffffff * Math.random()).toString(16);
-   var posx = (Math.random() * ($(document).width() - buttonsize)).toFixed();
-   var posy = (Math.random() * ($(document).height() - buttonsize)).toFixed();
+   let buttonsize = ((Math.random()*100) + 50).toFixed();
+   let color = '#'+ Math.round(0xffffff * Math.random()).toString(16);
+   let posx = (Math.random() * ($("#game").width())+$game.position().left);
+   let posy = (Math.random() * ($('#game').height())+$game.position().top);
+   console.log("PosX: "+posx+"   PosY: "+posy);
    $gamebutton.css({
+        'border-color': "rgb(216, 216, 216) rgb(209, 209, 209) rgb(186, 186, 186)",
+        'border-style': "solid",
+        'border-width': "1px",
+        'color' : "white",
         'position':'absolute',
         'left':posx+'px',
         'top':posy+'px',
