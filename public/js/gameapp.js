362c4e8bf2702e272c6f94adabced5fa20da35ac
Blake Harris <harrisblake85@gmail.com>
2017-12-18 12:07:33 -0700

    game changes universal tracking of color

--- public/js/gameapp.js
+++ public/js/gameapp.js
@@ -1,10 +1,4 @@
-console.log($);
-var socket = io();
-  socket.connect();
-  socket.on('myClick', function (data) {
-    console.log(data);
-    console.log(data.id);
-})
+
 $(() => {
   //onload
  const $gamebutton = $("<button>"+"Click Me To Score"+"</button>")
@@ -17,17 +11,14 @@ $(() => {
  })
  $game.append($gamebutton);
  $gamebutton.on("click",() => {
-   socket.emit('myClick', {id: event.target});
-   $gamebutton.hide();
-   let buttonsize = ((Math.random()*100) + 50).toFixed();
+
+   // $gamebutton.hide();
+   // let buttonsize = ((Math.random()*100) + 50).toFixed();
    let color = '#'+ Math.round(0xffffff * Math.random()).toString(16);
    let posx = (Math.random() * ($("#game").width())+$game.position().left-($gamebutton.width()/2));
    let posy = (Math.random() * ($('#game').height())+$game.position().top-$gamebutton.height());
    console.log("PosX: "+posx+"   PosY: "+posy);
    $gamebutton.css({
-        'border-color': "rgb(216, 216, 216) rgb(209, 209, 209) rgb(186, 186, 186)",
-        'border-style': "solid",
-        'border-width': "1px",
         'color' : "white",
         'position':'absolute',
         'left':posx+'px',
@@ -36,8 +27,30 @@ $(() => {
     }).show().fadeIn(100).delay(1000);
     $gamebutton.show();
 
- });
+   socket.emit('myClick', {
+     color: color,
+     posx:posx,
+     posy:posy
 
+   });
+ });
+ console.log($);
+ var socket = io();
+   socket.connect();
+   socket.on('myClick', function (data) {
+     // console.log(data);
+     // console.log(data.id);
+     $gamebutton.css({
+          'color' : "white",
+          'position':'absolute',
+          'left':data.posx+'px',
+          'top':data.posy+'px',
+          'background-color': data.color
+      }).show().fadeIn(100).delay(1000);
+      $gamebutton.show();
+
+
+ })
 
 
 
