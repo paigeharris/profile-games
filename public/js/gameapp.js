2c306b3505e973da491a7f285ab98dc03681ba7a
Blake Harris <harrisblake85@gmail.com>
2017-12-18 13:47:59 -0700

    scoreboard working

--- public/js/gameapp.js
+++ public/js/gameapp.js
@@ -3,6 +3,7 @@ $(() => {
   //onload
  const $gamebutton = $("<button>"+"Click Me To Score"+"</button>")
  const $game = $("#game")
+ const $scoreboard = $("<table>");
  $game.css({
    width:'800px',
    height:'600px',
@@ -10,7 +11,10 @@ $(() => {
    overflow:"hidden"
  })
  $game.append($gamebutton);
- let scores = {player1:0,player2:0}
+ $game.append($scoreboard)
+ let user = Math.round(0xffffff * Math.random()).toString(16);
+ let scores = {}
+ scores[user]=0;
  $gamebutton.on("click",() => {
 
    let color = '#'+ Math.round(0xffffff * Math.random()).toString(16);
@@ -25,8 +29,20 @@ $(() => {
         'background-color': color
     }).show().fadeIn(100).delay(1000);
     $gamebutton.show();
-    scores.player1++;
+    if (scores[user]!=null) {
+      scores[user]++;
+    }
+    else {
+      scores[user]=1;
+    }
+
     console.log(scores);
+    console.log("My Score: "+scores[user]);
+    $scoreboard.empty();
+    $scoreboard.append($("<thead>"+"</thead>").append($("<td>"+"User"+"</td>"),$("<td>"+"Score"+"</td>")));
+    for (let key in scores) {
+      $scoreboard.append($("<tr>").append($("<td>"+key+"</td>"),$("<td>"+scores[key]+"</td>")));
+    }
 
    socket.emit('myClick', {
      color: color,
@@ -41,7 +57,6 @@ $(() => {
  var socket = io();
    socket.connect();
    socket.on('myClick', function (data) {
-     data.scores.player2++;
      scores=data.scores;
      console.log(data.scores);
      $gamebutton.css({
@@ -52,7 +67,11 @@ $(() => {
           'background-color': data.color
       }).show().fadeIn(100).delay(1000);
       $gamebutton.show();
-
+      $scoreboard.empty();
+      $scoreboard.append($("<thead>"+"</thead>").append($("<td>"+"User"+"</td>"),$("<td>"+"Score"+"</td>")));
+      for (let key in scores) {
+        $scoreboard.append($("<tr>").append($("<td>"+key+"</td>"),$("<td>"+scores[key]+"</td>")))
+      }
 
  })
 
